f4ad8378c4080731f72dd1e19b326e80031e30db
ralpdexterbongato <rdc2898@gmail.com>
2018-05-18 11:32:22 +0800

    try fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -147,6 +147,6 @@ class AuthController extends Controller
   }
   public function findMatchedPeople(Request $request)
   {
-    return User::where(DB::raw("CONCAT(users.fname,' ',users.lname)"),'%'.$request->q.'%')->paginate(5);
+    return User::where(DB::raw("CONCAT(users.fname,' ',users.lname)"),'LIKE','%'.$request->q.'%')->paginate(5);
   }
 }
