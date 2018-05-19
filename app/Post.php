2ec170e023720cee1fd0780d0a0c5838cd03b17b
ralpdexterbongato <rdc2898@gmail.com>
2018-05-20 00:26:31 +0800

    time posted replaced by just now

--- app/Post.php
+++ app/Post.php
@@ -10,7 +10,7 @@ class Post extends Model
     {
 
       $timePosted = Carbon::createFromFormat('Y-m-d H:i:s', $date)->diffForHumans(null,true,true);
-      $timePosted = str_replace(['1s','2s','3s','4s','5s','6s','7s','8s','9s'], 'just now', $timePosted);
+      $timePosted = str_replace(['11s','12s','13s','14s','15s','16s','17s','18s','19s','20s','1s','2s','3s','4s','5s','6s','7s','8s','9s','10s'], 'just now', $timePosted);
       return $timePosted;
     }
     public function Poster()
