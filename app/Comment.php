05e2feb8cf9602c3d0a041d108c94216be4d380a
ralpdexterbongato <rdc2898@gmail.com>
2018-05-20 00:35:59 +0800

    time bug fix

--- app/Comment.php
+++ app/Comment.php
@@ -17,7 +17,8 @@ class Comment extends Model
     public function getUpdatedAtAttribute($date)
     {
     	$timeCommented = Carbon::createFromFormat('Y-m-d H:i:s',$date)->diffForHumans(null,true,true);
-      $timeCommented = str_replace(['1just now','2just now','3just now','4just now','5just now','6just now','7just now','8just now','9just now'], 'just now', $timeCommented);
+      $timeCommented = str_replace(['0s','1s','2s','3s','4s','5s','6s','7s','8s','9s','10s'], 'just now', $timeCommented);
+      $timeCommented = str_replace(['0just now','1just now','2just now','3just now','4just now','5just now','6just now','7just now','8just now','9just now'], 'just now', $timeCommented);
       return $timeCommented;
     }
     public function replies()
--- app/Post.php
+++ app/Post.php
@@ -10,8 +10,8 @@ class Post extends Model
     {
 
       $timePosted = Carbon::createFromFormat('Y-m-d H:i:s', $date)->diffForHumans(null,true,true);
-      $timePosted = str_replace(['1s','2s','3s','4s','5s','6s','7s','8s','9s','10s'], 'just now', $timePosted);
-      $timePosted = str_replace(['1just now','2just now','3just now','4just now','5just now','6just now','7just now','8just now','9just now'], 'just now', $timePosted);
+      $timePosted = str_replace(['0s','1s','2s','3s','4s','5s','6s','7s','8s','9s','10s'], 'just now', $timePosted);
+      $timePosted = str_replace(['0just now','1just now','2just now','3just now','4just now','5just now','6just now','7just now','8just now','9just now'], 'just now', $timePosted);
       return $timePosted;
     }
     public function Poster()
